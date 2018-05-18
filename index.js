c331c0a3581b63f48c5681215b641886d8eb4b95
alpha <827130441@qq.com>
2018-05-18 17:31:52 +0800

    测试静态服务器通过

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const http = require('http');
+const {makeStaticServer} = require('./src/static-server/index')
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  // 静态服务器模块
+  makeStaticServer(req, res, __dirname)
 });
+
+server.on('error', (err) => {
+  console.log(err)
+})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
